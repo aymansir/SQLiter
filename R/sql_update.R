ec72fac80f72e3c23811e1bb56b39a91987ecfdc
Connor Lawless <connor.lawless@hotmail.com>
2016-06-06 22:58:21 +0200

    My implementation of the update function - made it consistent with our other functions for the where function

--- R/sql_update.R
+++ R/sql_update.R
@@ -1,56 +1,32 @@
 #'Updates an existing data table
 #'
-#'@param table A string that is the name of the table
-#'@param data a matrix that's m is the bulk of observations and n is the number of features
-#'@param where the column that you are matching
-#'@param the value that is being matched
+#'@param tbl - the table you'd like to perform an update on 
+#'@param set - a list (or vector/matrix/data.frame) containing vectors of values you'd like to update the selected rows with. Each different column change should be an element. Each list element is a different query.
+#'@param where - a vector of where conditions for each of your updates
 #'
 #'@examples
-#'sql_update('Cars', car_data_frame, 'Car', 'honda')
+#'sql_update("table_1", list(c("c1 = 301", "c2 =307"), c("c1 =909")), c("c1=300","c1 =99"))
 #'@export
-
-library(stringr)
-sql_update <- function(table_name, data, where, where_equals) {
-  if (!exists("connection")) stop("There is no connection open.")
-  sqlUpdateBuilder(table_name, data, where, where_equals)
-}
-
-sqlUpdateBuilder <- function(table, data, where, where_equals) {
-
-  row_names <- names(data)
-
-  if(!(class(data) == 'data.frame' ||'matrix')) stop('Please use a data frame')
-
-  for (i in 1:nrow(data)) {
-    sql <- paste('update',table)
-    set_sql <- ' set '
-
-    sql_set = ''
-
-    for (j in 1:ncol(data)) {
-      sql_set <- paste(sql_set, row_names[j],'=\'', data[i,j], '\'', ', ', sep='')
-
-    }
-
-    if(i == nrow(data)) {
-      dataFrame <- paste(sql)
-    }
-    else {
-      dataFrame <- paste(sql, ' set ', sql_set, sep = '')
-
-    }
-    dataFrame <- paste(substring(dataFrame, 1, (nchar(dataFrame)-2)))
-    dataFrame <- paste(dataFrame,' where ', where, '=','\'', where_equals,'\'', ';', sep='' )
-    print(dataFrame)
-    results <- dbSendQuery(connection, dataFrame)
-
-
+sql_update <- function(tbl, set, where){
+  if(length(set)!=length(where)){
+    stop("Inconsistent number of queries requested (length of set and where not equal)")
   }
-
+  if(class(set)=="character"){
+    set <- list(set)
+  }
+  #puts it into a list if it was in a dataframe
+  else if(class(set)=="data.frame"){
+    set <- as.list(as.data.frame(t(set)))
+  }
+  #puts it into a list if it was a matrix
+  else if(class(set)=="matrix"){
+    set <- lapply(1:nrow(set), function(i) set[i,])
+  }
+  #loops through values and inserts all the values
+  for(i in 1:length(set)){
+    set_str <- paste(set[[i]], collapse = ",")
+    input <- sprintf("UPDATE %s SET %s WHERE %s;", tbl, set_str, where[i])
+    dbSendQuery(connection, input)
+  }
+  message("Successful update!")
 }
-
-
-
-
-
-
